6cf61c4b014c0bd1404d92eedbb4edcbdcd3d86f
Recai Cingöz <recai@rise-consulting.net>
2020-06-03 23:08:37 +0300

    Add project files.

---
+++ DelegateActionFunc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateActionFunc
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.WorkPerformed += Worker_WorkPerformed;
+            worker.WorkCompleted += Worker_WorkCompleted;
+            worker.DoWork(5, WorkType.Golf);
+
+            Console.ReadLine();
+        }
+
+
+        private static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Tamamlandi");
+        }
+
+        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"Burasi Calisti : {e.Hours} - {e.WorkType}");
+        }
+    }
+
+    public enum WorkType
+    {
+        Golf,
+        Meeting,
+        Reporting
+    }
+}
---
+++ DelegateActionFunc/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegateActionFunc
+{ 
+    public class Worker
+    {
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+        public virtual void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            if (WorkPerformed is EventHandler<WorkPerformedEventArgs> del)
+                del(this, new WorkPerformedEventArgs(hours, workType));
+
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            if (WorkCompleted is EventHandler eventHandler)
+                eventHandler(this, EventArgs.Empty);
+        }
+
+
+    }
+}
